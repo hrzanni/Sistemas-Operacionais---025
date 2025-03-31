22ee1e692eebe0669c4b41b388bee446e5cada6c
hrzanni <167593488+hrzanni@users.noreply.github.com>
2025-03-31 01:29:42 -0300

    Create read_file.c

---
+++ codes-System Calls/codesFiles/read_file.c
@@ -0,0 +1,31 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    int fd = open("myFile.txt", O_RDONLY);
+    if (fd < 0) {
+        perror("Error opening file for reading");
+        exit(EXIT_FAILURE);
+    }
+
+    char buffer[BUFSIZ + 1];
+    ssize_t bytesRead;
+
+    while ((bytesRead = read(fd, buffer, BUFSIZ)) > 0){
+        buffer[bytesRead] = '\0';
+        printf("%s", buffer);
+    }
+
+    if (bytesRead < 0) {
+        perror("Error reading file");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+
+    close(fd);
+    return 0;
+}
