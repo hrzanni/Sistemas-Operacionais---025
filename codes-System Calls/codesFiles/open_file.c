033fe3cc6cb704ef0fde55c72ae0c8dcb003ed48
hrzanni <167593488+hrzanni@users.noreply.github.com>
2025-03-31 01:28:50 -0300

    Create open_file.c

---
+++ codes-System Calls/codesFiles/open_file.c
@@ -0,0 +1,17 @@
+#include <fcntl.h>     
+#include <unistd.h>    
+#include <stdio.h>     
+#include <stdlib.h>    
+
+int main() {
+    int fd;
+
+    fd = open("myFile.txt", O_CREAT | O_WRONLY | O_TRUNC, 0644);
+    if (fd < 0) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    close(fd);
+    return 0;
+}
