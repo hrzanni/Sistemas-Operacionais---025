816127f9cd3bf690869ce0ee944671f79509ccb6
hrzanni <167593488+hrzanni@users.noreply.github.com>
2025-03-31 01:30:07 -0300

    Create write_file.c

---
+++ codes-System Calls/codesFiles/write_file.c
@@ -0,0 +1,27 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+    int fd;
+    char *message = "writing in my file!!\n";
+
+    fd = open("myFile.txt", O_WRONLY | O_APPEND);
+    if (fd < 0) {
+        perror("Error opening file for writing");
+        exit(EXIT_FAILURE);
+    }
+
+    if (write(fd, message, strlen(message)) < 0) {
+        perror("Error writing to file");
+        close(fd);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Message written to 'myFile.txt'.\n");
+
+    close(fd);
+    return 0;
+}
