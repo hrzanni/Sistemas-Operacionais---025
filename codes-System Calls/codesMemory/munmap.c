5995b7600a87bc9964a9eea292ebb7fab31c7167
hrzanni <167593488+hrzanni@users.noreply.github.com>
2025-03-31 02:00:53 -0300

    Update munmap.c

--- codes-System Calls/codesMemory/munmap.c
+++ codes-System Calls/codesMemory/munmap.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
+#include <string.h>
+
+int main() {
+   
+    size_t length = 4096;
+    void *addr;
+
+    addr = mmap(NULL, length, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+    
+    if (addr == MAP_FAILED) {
+        perror("mmap");
+        exit(1);
+    }
+
+    strcpy((char*)addr, "Memória alocada com mmap() e manipulada!");
+
+    printf("Conteúdo na memória mapeada: %s\n", (char*)addr);
+
+    if (munmap(addr, length) == -1) {
+        perror("munmap");
+        exit(1);
+    }
+
+    printf("Memória desalocada com sucesso!\n");
+
+    return 0;
+}
 
