fc17a3de56912a6d6685863eef6d8c340ec37d51
hrzanni <167593488+hrzanni@users.noreply.github.com>
2025-03-31 13:52:20 -0300

    Update CPU_bound.c

--- codes-System Calls/IO bound & CPU bound/CPU_bound.c
+++ codes-System Calls/IO bound & CPU bound/CPU_bound.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+#define ITERATIONS 10000000000LL
+
+int main() {
+    __uint128_t sum = 0;    
+
+    printf("Iniciando cálculos...\n");
+    for (__uint128_t i = 1; i <= ITERATIONS; i++) {
+        sum += i;
+    }
+    
+    unsigned long long high = (unsigned long long)(sum >> 64);  // Parte mais alta
+    unsigned long long low = (unsigned long long)(sum);  // Parte mais baixa
+
+    printf("%llu%llu\n", high, low);
+    
+    return 0;
+}
